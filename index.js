c19764c77f4956bcaa98586c9e6f2bfee31f1d32
DamianL96 <damianlambrecht@gmail.com>
2024-05-06 18:41:58 -0300

    barra de navegacion aparte

--- index.js
+++ index.js
@@ -2,21 +2,15 @@ const burguer = document.querySelector(".Burguer");
 
 const navBarLinks = document.querySelector(".navbar-links");
 
-var paginaActual = new URL(window.location.href).pathname;
-var navLinks = document.querySelectorAll('.navbar-links a');
+// var paginaActual = new URL(window.location.href).pathname;
+// var navLinks = document.querySelectorAll('.navbar-links a');
 
-fetch('components/nav.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('nav').innerHTML = data;
-  });
-
-navLinks.forEach( link=>{
-  var linkPath = new URL(link.href).pathname;
-  if(linkPath === paginaActual){
-    link.classList.add('pagina-actual');
-  }
-});
+// navLinks.forEach( link=>{
+//   var linkPath = new URL(link.href).pathname;
+//   if(linkPath === paginaActual){
+//     link.classList.add('pagina-actual');
+//   }
+// });
 
 
 burguer.addEventListener("click", () =>{
