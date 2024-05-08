6c634b2d95210bd98a8c35f5f1e67c99e8f60243
DamianL96 <damianlambrecht@gmail.com>
2024-05-08 09:13:18 -0300

    limpiado de comentarios

--- scripts/barra.js
+++ scripts/barra.js
@@ -1,105 +1,82 @@
 
- const productos = document.querySelectorAll('.tarjeta');
+const productos = document.querySelectorAll('.tarjeta');
 
- 
- productos.forEach(tarjeta => {
-     tarjeta.addEventListener('click', () => {
-         
-         const nombre = tarjeta.getAttribute('data-nombre');
-         const cantidad = parseInt(tarjeta.getAttribute('data-cantidad'));
-         const precio = parseInt(tarjeta.getAttribute('data-precio'));
+productos.forEach(tarjeta => {
+  tarjeta.addEventListener('click', () => {
 
-        
-         agregarProductoAFactura(nombre, cantidad, precio);
-     });
- });
+    const nombre = tarjeta.getAttribute('data-nombre');
+    const cantidad = parseInt(tarjeta.getAttribute('data-cantidad'));
+    const precio = parseInt(tarjeta.getAttribute('data-precio'));
+
+    agregarProductoAFactura(nombre, cantidad, precio);
+  });
+});
 
 
 function agregarProductoAFactura(nombre, cantidad, precio) {
-   
-    const fila = document.createElement('tr');
 
-    
-    fila.innerHTML = `
+  const fila = document.createElement('tr');
+
+  fila.innerHTML = `
     <td>${nombre}</td>
     <td>${cantidad}</td>
     <td>$${precio.toFixed(2)}</td>
     <td>$${(cantidad * precio).toFixed(2)}</td>
     `;
 
-    
-    document.querySelector('#factura tbody').appendChild(fila);
+  document.querySelector('#factura tbody').appendChild(fila);
 
-   
-    actualizarTotalFactura(cantidad * precio);
-
-    
+  actualizarTotalFactura(cantidad * precio);
 }
 
- 
- function actualizarTotalFactura(precioProducto) {
-     
-     const totalElemento = document.getElementById('total');
-     
-     let total = parseFloat(totalElemento.innerText.replace('$', ''));
-     
-     total += precioProducto;
-     totalElemento.innerText = `$${total.toFixed(2)}`;
- }
 
+function actualizarTotalFactura(precioProducto) {
 
+  const totalElemento = document.getElementById('total');
+
+  let total = parseFloat(totalElemento.innerText.replace('$', ''));
+
+  total += precioProducto;
+  totalElemento.innerText = `$${total.toFixed(2)}`;
+}
 
 
 document.getElementById('abrirMesa').addEventListener('click', () => {
-   
-    reiniciarFactura();
+  reiniciarFactura();
 });
 
 
 document.getElementById('cerrarMesa').addEventListener('click', () => {
-   
-    const totalVenta = parseFloat(document.getElementById('total').innerText.replace('$', ''));
-
-    
-    
-    
+  const totalVenta = parseFloat(document.getElementById('total').innerText.replace('$', ''));
 });
 
 
 function reiniciarFactura() {
-   
-    document.querySelector('#factura tbody').innerHTML = '';
-
-   
-    document.getElementById('total').innerText = '$0.00';
+  document.querySelector('#factura tbody').innerHTML = '';
+  document.getElementById('total').innerText = '$0.00';
 }
 
 
 document.getElementById('abrirMesa').addEventListener('click', () => {
-   
-    document.getElementById('cuadro').style.display = 'block';
-  
-    document.getElementById('mensaje').innerText = 'Venta Restaurada';
-   
-    document.getElementById('totalVenta').style.display = 'none';
+  document.getElementById('cuadro').style.display = 'block';
+  document.getElementById('mensaje').innerText = 'Venta Restaurada';
+  document.getElementById('totalVenta').style.display = 'none';
 });
 
 
 document.getElementById('cerrarMesa').addEventListener('click', () => {
-   
-    const totalVenta = parseFloat(document.getElementById('total').innerText.replace('$', ''));
-    
-    document.getElementById('cuadro').style.display = 'block';
-   
-    document.getElementById('mensaje').innerText = 'Venta cerrada';
-   
-    document.getElementById('totalVenta').style.display = 'block';
-    document.getElementById('totalVenta').innerText = `Total de la venta: $${totalVenta.toFixed(2)}`;
-    reiniciarFactura();
+
+  const totalVenta = parseFloat(document.getElementById('total').innerText.replace('$', ''));
+
+  document.getElementById('cuadro').style.display = 'block';
+  document.getElementById('mensaje').innerText = 'Venta cerrada';
+  document.getElementById('totalVenta').style.display = 'block';
+  document.getElementById('totalVenta').innerText = `Total de la venta: $${totalVenta.toFixed(2)}`;
+
+  reiniciarFactura();
 });
 
 
 document.getElementById('cerrarCuadro').addEventListener('click', () => {
-   
-    document.getElementById('cuadro').style.display = 'none';
+  document.getElementById('cuadro').style.display = 'none';
 });
