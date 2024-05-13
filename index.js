7cf27af3b1d635400badebf7d7571b305b8f647d
DamianL96 <damianlambrecht@gmail.com>
2024-05-13 20:45:15 -0300

    comienzo de login

--- index.js
+++ index.js
@@ -5,6 +5,54 @@ burguer.addEventListener("click", () =>{
     navBarLinks.classList.toggle("active");
 });
 
+
+const inicioSesion = document.querySelector("#inicioSesion");
+const username = 'abc123';
+const password = 'abc123';
+
+inicioSesion.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  let user = document.querySelector('#username');
+  let pass = document.querySelector('#password');
+  console.log(inicioSesion);
+  if (campoVacio(user)) {
+    document.querySelector('#userRequired').classList.replace('hidden','show');
+  }else{
+    document.querySelector('#userRequired').classList.replace('show','hidden');
+  }
+
+  if(campoVacio(pass)){
+    document.querySelector('#passRequired').classList.replace('hidden','show');
+  }else{
+    document.querySelector('#passRequired').classList.replace('show','hidden');
+  }
+
+   if(campoVacio(user) && campoVacio(pass)){
+   }
+
+})
+
+function campoVacio(input){
+  if(input.value === ""){
+    return true;
+  }
+  return false;
+}
+
+function validar(input){
+  if(input.name === "username"){
+    if(input.value != username){
+      return false;
+    }
+  }
+  if(input.name === "password"){
+    if(input.value != password){
+      return false;
+    }
+  }
+  return true;
+}
+
 const navBarLinks = document.querySelector(".navbar-links");
 
 document.querySelectorAll('.nav-link').forEach(link => {
