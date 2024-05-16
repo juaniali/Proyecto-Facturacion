8b7333eee242175d8c993a82957e81e70a28461c
DamianL96 <damianlambrecht@gmail.com>
2024-05-16 17:32:26 -0300

    actualizacion de buscar producto

--- scripts/mesas.js
+++ scripts/mesas.js
@@ -34,6 +34,8 @@ buttonAbrirMesa.addEventListener('click', (e)=>{
 //Buscar Procto------------------------------------------
 let productos = [];
 
+
+
 async function consumirDb(){
   try {
     const db = await fetch('./../db/productos.json');
@@ -47,14 +49,53 @@ async function consumirDb(){
 consumirDb();
 const buscar = document.querySelector("#buscar-producto");
 
+const contenedorProductos= document.querySelector("#contenedor-productos");
+const fragmento= document.createDocumentFragment();
+
+
 buscar.addEventListener("keyup", () => {
   const valorBuscado = buscar.value.toLowerCase();
   const resultados =  productos.filter( producto => producto.nombre.toLowerCase().includes(valorBuscado) );
+  limpiarProductos();
   mostrarProductos(resultados);
 });
 
 function mostrarProductos(productos){
+  
   productos.forEach( producto =>{
-    console.log( "Nombre", producto.nombre, "Precio", producto.precio, "img", producto.img );
+    let li= document.createElement("li");
+    li.className= "tarjeta";
+    
+    let nombre= document.createElement("p");
+    nombre.textContent= producto.nombre;
+    li.appendChild(nombre);
+    
+    let precio= document.createElement("p");
+    precio.textContent= producto.precio;
+    li.appendChild(precio);
+
+    let imagen= document.createElement("img");
+    imagen.src= producto.img;
+    imagen.className= "imagen-tarjeta";
+    li.appendChild(imagen);
+
+    fragmento.appendChild(li);
   });
+  contenedorProductos.appendChild(fragmento);
 }
+
+function limpiarProductos(){
+  while(contenedorProductos.firstChild){
+    contenedorProductos.removeChild(contenedorProductos.firstChild);
+  }
+}
+
+/*
+<ul class="productos-tarjetas">
+  <li class="tarjeta" data-nombre="CrossaintJQ" data-cantidad="1" data-precio="1000">
+    <p>CrossaintJQ</p>
+    <h3>$1000</h3>
+    <img class="imagen-tarjeta" src="./../img/petr-magera-Dh9c2VBi8S0-unsplash.jpg" alt="">
+  </li>
+</ul>
+*/
