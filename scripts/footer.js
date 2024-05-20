6397ed20f0357221f993c52b722abf2dc5d0c77b
DamianL96 <damianlambrecht@gmail.com>
2024-05-20 17:08:28 -0300

    footer a todas las pags

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,26 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Seleccionar elementos del DOM
+  const botonCentroAyuda = document.querySelector('.boton-centro-ayuda');
+  const modalAyuda = document.querySelector('.modal-ayuda');
+  const botonCerrar = document.querySelector('.boton-cerrar');
 
-const botonCentroAyuda = document.querySelector('.boton-centro-ayuda');
-const modalAyuda = document.querySelector('.modal-ayuda');
-const botonCerrar = document.querySelector('.boton-cerrar');
-
-// Mostrar el modal cuando se presiona el botón "Centro de ayuda"
-botonCentroAyuda.addEventListener('click', () => {
+  // Mostrar el modal cuando se presiona el botón "Centro de ayuda"
+  botonCentroAyuda.addEventListener('click', () => {
     modalAyuda.style.display = 'block';
-});
+  });
 
-
+// Cerrar el modal cuando se presiona la "x"
 botonCerrar.addEventListener('click', () => {
     modalAyuda.style.display = 'none';
-});
+  });
 
-// Cerrar el modal cuando se hace clic fuera del contenido del modal
-window.addEventListener('click', (event) => {
+  // Cerrar el modal cuando se hace clic fuera del contenido del modal
+  window.addEventListener('click', (event) => {
     if (event.target === modalAyuda) {
-        modalAyuda.style.display = 'none';
+      modalAyuda.style.display = 'none';
     }
-});
+  });
 
-
+// Manejar el envío del formulario
 const formularioAyuda = document.querySelector('.formulario-ayuda');
 formularioAyuda.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -30,4 +31,6 @@ formularioAyuda.addEventListener('submit', (event) => {
     alert('Formulario enviado con éxito!');
    
     modalAyuda.style.display = 'none';
+  });
+
 });
