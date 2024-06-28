3c1acee5d3b713636a807120434e57f340b3fd9c
DamianL96 <damianlambrecht@gmail.com>
2024-06-28 20:36:43 -0300

    instalando express

---
+++ src/db/dbConnection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+const conn = mysql.createPool({
+  host :'mysql-lambrechtdatabase.alwaysdata.net',
+  user :'366567_admin',
+  password:'366567Admin',
+  database:'lambrechtdatabase_cafe',
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = {
+  conn : conn.promise()
+}
