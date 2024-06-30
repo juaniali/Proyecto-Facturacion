f74e97e3ea77a178796c866188fc6587cdcfd2ba
nicolas <xamuscero@gmail.com>
2024-06-30 18:37:29 -0300

    cambios pagina datos antes de morter

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const controladores = require('../controllers/productController');
+const multer = require ("multer")
+const storage = multer.diskStorage({
+    destination: (req, file , cb) =>{cb(null, "pubic/img/ ")},
+    filename:(req, file, cb) =>{console.log(file),cb(null, Date.now() + "_" + file.originalname)
+    }
+})
+const uploadFile = multer({storage})
+
 
 router.get('/pages/carga-datos', controladores.getListado);
-router.post('/pages/carga-datos', controladores.crearRegistro);
+router.post('/pages/carga-datos',controladores.crearRegistro);
 router.get('/modificar/:num', controladores.getModificar);
 router.patch('/modificar', controladores.actualizar);
 router.delete('/pages/carga-datos', controladores.delete);
