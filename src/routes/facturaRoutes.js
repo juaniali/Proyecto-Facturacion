90ab57c3384a1162ba462b1c43f697f5e0aa49ab
Leo <124378386+leonel009@users.noreply.github.com>
2024-07-04 15:34:56 -0300

    rutas de facturacion incompletas

--- src/routes/facturaRoutes.js
+++ src/routes/facturaRoutes.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const controladores = require('./../controllers/facturaController');
+const auth = require('./../config/auth');
 
-router.get('/listado', controladores.getFacturas);
-//router.post('/cargar',controladores.crearFactura);
+// get
+router.get('/pages/balance', controladores.getFacturas);
+router.get('/listado', controladores.getDetallesByIdFactura);
+router.get('/', controladores.index);
+
+// cargar
+router.post('/cargar', controladores.crearFactura);
+router.post('/cargar', controladores.crearDetalle);
+
+// modificar
+router.patch('/', controladores.actualizarFactura);
+router.patch('/', controladores.actualizarDetalle);
+
+// borrar
+router.delete('/', controladores.deleteFactura);
+router.delete('/', controladores.deleteDetalle);
 
 module.exports = router;
